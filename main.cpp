3a33ecc1c7aa6c1b5270d7c1cea43dda3886a3c7
126Florian <91480650+126Florian@users.noreply.github.com>
2021-11-13 20:51:38 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int n=0, a=0, b=1, c=a+b;
+    cin>>n;
+    cout<<1<<endl;
+    for(int i=0; i<(n-1); i++){
+        c=a+b;
+        a=b;
+        b=c;
+        cout<<c<<endl;
+    }
+    return 0;
 }
